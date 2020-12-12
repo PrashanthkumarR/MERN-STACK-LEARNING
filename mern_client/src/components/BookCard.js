83b108f0371b8719344cf24489511517fc29b903
AzureAD\PrashanthKumar <prashanthninu@gmail.com>
2020-12-12 14:57:14 +0530

    first commit to add client and server fo mern lerning

---
+++ mern_client/src/components/BookCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+const BookCard = (props) => {
+  const book = props.book;
+
+  return (
+    <div className="card-container">
+      <img height="175px"
+        src="https://www.gstatic.com/webp/gallery/1.jpg"
+        alt=""
+      />
+      <div className="desc">
+        <h2>
+          <Link to={`/show-book/${book._id}`}>{book.title}</Link>
+        </h2>
+        <h3>{book.author}</h3>
+        <p>{book.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BookCard;
